b28fa27150209c4a71ca0be52c4a0aaa3c2fc072
Justin Frevert <justinfrevert@gmail.com>
2023-03-25 01:45:34 -0400

    Prover example sends proof

--- pallets/template/src/lib.rs
+++ pallets/template/src/lib.rs
@@ -14,14 +14,15 @@ mod tests;
 #[cfg(feature = "runtime-benchmarks")]
 mod benchmarking;
 
-mod risc0;
+mod common;
 
 #[frame_support::pallet]
 pub mod pallet {
 	use frame_support::pallet_prelude::*;
 	use frame_system::pallet_prelude::*;
 	use risc0_zkvm::Receipt;
-	use crate::risc0::get_seal;
+	use crate::common::{MULTIPLY_IMAGE_ID, MULTIPLY_JOURNAL};
+	use sp_std::vec::Vec;
 
 	#[pallet::pallet]
 	#[pallet::generate_store(pub(super) trait Store)]
@@ -38,16 +39,14 @@ pub mod pallet {
 	#[pallet::event]
 	#[pallet::generate_deposit(pub(super) fn deposit_event)]
 	pub enum Event<T: Config> {
-		/// Event documentation should end with an array that provides descriptive names for event
-		/// parameters. [something, who]
-		SomethingStored { something: u32, who: T::AccountId },
+		/// The seal was verified
+		VerificationSuccess
 	}
 
-	// Errors inform users that something went wrong.
 	#[pallet::error]
 	pub enum Error<T> {
-		/// Error names should be descriptive.
-		NoneValue,
+		/// The seal could not be verified
+		FailedVerification,
 	}
 
 	#[pallet::call]
@@ -57,18 +56,13 @@ pub mod pallet {
 		// Risc0 factors example
 		pub fn send_factors_receipt(
 			origin: OriginFor<T>,
+			seal: Vec<u32>
 		) -> DispatchResult {
-			use sp_std::vec;
 			ensure_signed(origin)?;
-
-			let seal = get_seal();
-			let receipt = Receipt::new(&[135, 1, 0, 0, 0, 0, 0, 0], &seal);
-			let image_id: risc0_zkvm::sha::Digest = risc0_zkvm::sha::Digest::new([133937908, 3727778855, 2100795079, 4075041621, 3309653911, 1482515540, 3335215972, 449739275]);
-
-			let verif_result  = receipt.verify(&image_id).expect("Verification failed possibly due to incorrect parameters");
-
+			let receipt = Receipt::new(&MULTIPLY_JOURNAL, &seal);
+			receipt.verify(&MULTIPLY_IMAGE_ID).map_err(|_| Error::<T>::FailedVerification)?;
+			Self::deposit_event(Event::<T>::VerificationSuccess);
 			Ok(())
 		}
-
 	}
 }
